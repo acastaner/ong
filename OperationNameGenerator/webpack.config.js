778e375a022826ab6f621cec2dab0c724a8f1be6
Arnaud Castaner <arnaud.castaner@outlook.com>
2017-04-05 06:50:58 +0200

    Update .gitignore; Add webpack.config.js;

---
+++ OperationNameGenerator/webpack.config.js
@@ -0,0 +1,8 @@
+﻿const webpack = require("webpack");
+const config = require("folke-webpack-config");
+
+config.resolve.alias = {
+    "knockout": require.resolve("knockout")
+};
+
+module.exports = config;
