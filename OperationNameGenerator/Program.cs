49099e461e17fccb3999ded2666f2fcf784bf1c6
Arnaud Castaner <arnaud.castaner@protonmail.com>
2017-04-19 11:52:24 +0200

    Can now configure listening port from appsettings.json;

--- OperationNameGenerator/Program.cs
+++ OperationNameGenerator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace OperationNameGenerator
 {
@@ -8,12 +9,19 @@ namespace OperationNameGenerator
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddJsonFile("appsettings.Local.json", optional: true)
+                    .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
+                .UseUrls("http://0.0.0.0:" + config["Data:ListeningPort"])
                 .Build();
 
             host.Run();
