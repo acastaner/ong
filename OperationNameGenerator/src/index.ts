ae66365c3ccc8a6f8cbc1ebe518b4cb595e4536a
Arnaud Castaner <arnaud.castaner@protonmail.com>
2017-04-19 19:07:59 +0200

    Add addLoggedRoute; admin component;

--- OperationNameGenerator/src/index.ts
+++ OperationNameGenerator/src/index.ts
@@ -7,16 +7,36 @@ import * as identity from 'folke-identity';
 
 import { HomeViewModel } from './components/home';
 import { AboutViewModel } from './components/about';
+import { AdminViewModel } from './components/admin/admin';
 
 declare function require(name: string): string;
 require('./index.less');
 
 const { folke, defaultMenu } = defaultApp.register(services.services)
 
+function addLoggedRoute(pattern: string, view: string) {
+    folke.addRoute(pattern, parameters => {
+        identity.authentication.getLogged().then(logged => {
+            if (logged) {
+                return folke.goToView(view, parameters);
+            }
+            else {
+                location.assign('#identity-login');
+                return undefined;
+            }
+        });
+    });
+}
+
 ko.components.register('home', { viewModel: HomeViewModel, template: require('./components/home.html') });
 ko.components.register('about', { viewModel: HomeViewModel, template: require('./components/about.html') });
+ko.components.register('admin', { viewModel: AdminViewModel, template: require('./components/admin/admin.html') });
 
 folke.addRoute('', 'home');
 folke.addRoute('about', 'about');
+folke.addRoute('logout', parameters => identity.authentication.logout().then(() => location.assign('')));
+addLoggedRoute('admin', 'admin');
+
 
 folke.start();
+
